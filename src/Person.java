acd3eab221b8feefff7377bd457898d8da737520
BartekBurzynski <bartlomiej.burzynski@wp.pl>
2019-08-11 17:49:21 +0200

    Utworzenie PoprawneOsoby

---
+++ src/Person.java
@@ -0,0 +1,63 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int pesel;
+
+    public Person(String firstName, String lastName, int age, int pesel) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.pesel = pesel;
+
+        if (firstName == null) {
+            throw new NumberFormatException();
+        }
+        if (lastName == null) {
+            throw new NameUndefinedException();
+        }
+        if (firstName.length() < 2) {
+            throw new NumberFormatException();
+        }
+        if (lastName.length() < 2) {
+            throw new NumberFormatException();
+        }
+        if (age < 1) {
+            throw new IncorrectAgeException();
+        }
+
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(int pesel) {
+        this.pesel = pesel;
+    }
+}
